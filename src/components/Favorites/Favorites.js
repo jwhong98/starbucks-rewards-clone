a4353b16c972a45d49c390571b71529809363138
jwhong <jwhong98@gmail.com>
2022-04-13 15:34:01 -0700

    favorites tabs toggleable + hover effects

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,31 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Favorites.module.scss";
 import FavoritesCard from "./FavoritesCard";
-import { reward2 } from "./data";
+import { data } from "./data";
 
 const Favorites = () => {
+  const [display, setDisplay] = useState(1);
+
+  const onClickHandler = (e) => {
+    const btns = document.querySelectorAll("button");
+    btns.forEach((btn) => {
+      btn.classList.remove(classes.active);
+    });
+    e.target.classList.add(classes.active);
+    setDisplay(Number(e.target.value));
+  };
   return (
     <section className={classes.favorites}>
       <h2>Get your favorites for free</h2>
 
       <div className={classes.favorites__content}>
-        <button className={classes.tablinks}>
+        <button className={classes.tablinks} value="0" onClick={onClickHandler}>
           25<span>★</span>
         </button>
-        <button className={`${classes.tablinks} ${classes.active}`}>
+        <button
+          className={`${classes.tablinks} ${classes.active}`}
+          value="1"
+          onClick={onClickHandler}
+        >
           50<span>★</span>
         </button>
-        <button className={classes.tablinks}>
+        <button className={classes.tablinks} value="2" onClick={onClickHandler}>
           150<span>★</span>
         </button>
-        <button className={classes.tablinks}>
+        <button className={classes.tablinks} value="3" onClick={onClickHandler}>
           200<span>★</span>
         </button>
-        <button className={classes.tablinks}>
+        <button className={classes.tablinks} value="4" onClick={onClickHandler}>
           400<span>★</span>
         </button>
       </div>
-      <FavoritesCard {...reward2} />
+      <FavoritesCard {...data[display]} />
     </section>
   );
 };
--- src/components/Favorites/data.js
+++ src/components/Favorites/data.js
@@ -1,35 +1,37 @@
-export const reward1 = {
-  id: 1,
-  img: require("../../images/25.webp"),
-  title: "Customize your drink",
-  content:
-    "make your drink just right with an extra espresso shot, dairy substitute or a dash of your favorite syrup.",
-};
-export const reward2 = {
-  id: 2,
-  img: require("../../images/50.webp"),
-  title: "Brewed hot coffee, bakery item or hot tea",
-  content:
-    "Pair coffee cake or an almost croissant with your fresh cup of hot brew.",
-};
-export const reward3 = {
-  id: 3,
-  img: require("../../images/150.webp"),
-  title: "Handcrafted drink, hot breakfast or parfait",
-  content:
-    "Have a really good morning with a breakfast sandwich, oatmeal or your favorite drink.",
-};
-export const reward4 = {
-  id: 4,
-  img: require("../../images/200.webp"),
-  title: "Salad, sandwich, or protein box",
-  content:
-    "Nourish your day with a hearty Chipotle Chicken Wrap or Eggs & Cheese Protein Box.",
-};
-export const reward5 = {
-  id: 5,
-  img: require("../../images/400.webp"),
-  title: "Select merchandise or at-home coffee",
-  content:
-    "Takehome a signature cup, a bag of coffee or your choice of select coffee accessories.",
-};
+export const data = [
+  {
+    id: 1,
+    img: require("../../images/25.webp"),
+    title: "Customize your drink",
+    content:
+      "make your drink just right with an extra espresso shot, dairy substitute or a dash of your favorite syrup.",
+  },
+  {
+    id: 2,
+    img: require("../../images/50.webp"),
+    title: "Brewed hot coffee, bakery item or hot tea",
+    content:
+      "Pair coffee cake or an almost croissant with your fresh cup of hot brew.",
+  },
+  {
+    id: 3,
+    img: require("../../images/150.webp"),
+    title: "Handcrafted drink, hot breakfast or parfait",
+    content:
+      "Have a really good morning with a breakfast sandwich, oatmeal or your favorite drink.",
+  },
+  {
+    id: 4,
+    img: require("../../images/200.webp"),
+    title: "Salad, sandwich, or protein box",
+    content:
+      "Nourish your day with a hearty Chipotle Chicken Wrap or Eggs & Cheese Protein Box.",
+  },
+  {
+    id: 5,
+    img: require("../../images/400.webp"),
+    title: "Select merchandise or at-home coffee",
+    content:
+      "Takehome a signature cup, a bag of coffee or your choice of select coffee accessories.",
+  },
+];
